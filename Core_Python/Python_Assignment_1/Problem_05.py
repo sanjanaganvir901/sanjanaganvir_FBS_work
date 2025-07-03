8e8737bff2cf165e63786ca0423be576b2515b65
sanjana <158440732+sanjanaganvir901@users.noreply.github.com>
2025-07-03 19:45:28 +0530

    Update Problem_05.py

--- Core_Python/Python_Assignment_1/Problem_05.py
+++ Core_Python/Python_Assignment_1/Problem_05.py
@@ -2,4 +2,4 @@ p = int(input("Enter a Principle : "))
 t = int(input("Enter a Time : "))
 r = int(input("Enter a Rate : "))
 CI = p * (1 + r/100) ** t - p
-print(f"Compound Interest is : {CI}")
+print("Compound Interest is : ",CI)
