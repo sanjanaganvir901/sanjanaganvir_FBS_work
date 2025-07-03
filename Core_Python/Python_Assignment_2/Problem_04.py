c3345bac114e5103a5fc229c2bd5df90bfd8efe3
sanjana <158440732+sanjanaganvir901@users.noreply.github.com>
2025-07-03 19:53:37 +0530

    Update Problem_04.py

--- Core_Python/Python_Assignment_2/Problem_04.py
+++ Core_Python/Python_Assignment_2/Problem_04.py
@@ -4,4 +4,4 @@ base = int(input("Enter a base :"))
 height = int(input("Enter a height :"))
 area1 =  0.5 * base * height
 area2 = length * breadth
-print(f"Area of rectangle : {area1} and Area of rectangle : {area2}")
+print("Area of rectangle : ",area1, "Area of rectangle : ",area2)
