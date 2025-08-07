28d857c0f813808ddcadc0cb645121c716d05626
sanjana <158440732+sanjanaganvir901@users.noreply.github.com>
2025-08-08 00:12:21 +0530

    Update program_01.py

--- Core_Python/Python_Test/Test_03/program_01.py
+++ Core_Python/Python_Test/Test_03/program_01.py
@@ -1,28 +1,3 @@
-# def is_prime(num):
-#     if num <= 1:
-#         return False
-#     for i in range(2, int(num**0.5)+1):
-#         if num % i == 0:
-#             return False
-#     return True
-
-# def get_first_n_primes(n):
-#     primes = []
-#     num = 2
-#     while len(primes) < n:
-#         if is_prime(num):
-#             primes.append(num)
-#         num += 1
-#     return primes
-
-# n = int(input("Enter how many prime numbers you want: "))
-# prime_list = get_first_n_primes(n)
-
-# print("First", n, "prime numbers are:")
-# for prime in prime_list:
-#     print(prime, end=" ")
-
-
 n = int(input("Enter n: "))
 count = 0
 num = 2
@@ -33,4 +8,5 @@ while count < n:
     else:
         print(num, end=" ")
         count += 1
+
     num += 1
